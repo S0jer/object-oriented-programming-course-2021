16080e08ba6d10fec89ead0461658f19675b1244
S0jer <ppjaskowiec@gmail.com>
2022-04-12 13:46:14 +0200

    fixed gradle and .fxml scenes

--- src/main/java/agh/ics/oop/gui/Controller.java
+++ src/main/java/agh/ics/oop/gui/Controller.java
@@ -3,6 +3,7 @@ package agh.ics.oop.gui;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 
 
@@ -10,6 +11,8 @@ import java.util.Arrays;
 
 public class Controller {
 
+    @FXML
+    private Label askForArguments;
     @FXML
     private TextField myData;
     @FXML
@@ -19,11 +22,20 @@ public class Controller {
     String[] arguments;
 
 
-    public void Start(ActionEvent event){
+    public void Start(ActionEvent event) {
         try {
+            boolean correct = true;
             arguments = myData.getText().split("");
-            System.out.println(Arrays.toString(arguments));
-        } catch (Exception e){
+            correct = false;
+            for (String c : arguments) {
+                if (!c.equals("f") || !c.equals("b") || !c.equals("r") || !c.equals("l")) {
+                    correct = true;
+                }
+            }
+            if (correct) {
+                askForArguments.setText("             Enter arguments, only f, b, r, l letters:");
+            }
+        } catch (Exception e) {
             System.out.println(e);
         }
     }
